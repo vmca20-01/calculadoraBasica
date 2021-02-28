ea8a554127562d478b65b1db4c4e97594f1a26e0
Victor Manuel <VMCA20>
2021-02-28 17:29:03 -0600

    Add project files.

---
+++ calculadoraBasica/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace calculadoraBasica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Opciones de Calculadora"+
+                          "\n"+
+                          "\n1.Suma"+
+                          "\n"+
+                          "\nDigite la opcioncion a ejecutar : ");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
